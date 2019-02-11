dae2ed5312a81845e7c439852ed04df931608af2
Sze Wa, Wong <szewa.wong@connect.polyu.hk>
2019-02-11 14:56:41 +0800

    added jsconfig

--- src/App.js
+++ src/App.js
@@ -53,6 +53,7 @@ const PresenterPage = require('pages/aframeEditor/presenterPage').default;
 // const AsyncPresenterPage = asyncLoadingComponent(() => import('pages/aframeEditor/presenterPage'));
 //const AsyncTestSaveLoad = asyncLoadingComponent(() => import('pages/TestSaveLoad'));
 const AsyncTestFileExplorer = asyncLoadingComponent(() => import('pages/TestFileExplorer/TestFileExplorer'));
+const AsyncProjectListPage = asyncLoadingComponent(() => import('pages/ProjectListPage'));
 
 class App extends Component {
   constructor(props) {
@@ -78,6 +79,7 @@ class App extends Component {
             {/* <Route exact path={routes.presenter} render={() => <AsyncPresenterPage />} /> */}
             <Route exact path={routes.presenter} component={PresenterPage} />
             {/* <Route exact path={routes.viewer} component={ViewerPage} /> */}
+            <Route exact path={routes.projectList} component={AsyncProjectListPage} />
             <Redirect to={routes.editor} />
           </Switch>
         </div>
---
+++ src/pages/ProjectListPage.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+
+import './ProjectListPage.css';
+
+
+class ProjectListPage extends Component {
+  render() {
+    return (
+      <div>
+        Project List Page
+      </div>
+    );
+  }
+}
+
+export default ProjectListPage;
