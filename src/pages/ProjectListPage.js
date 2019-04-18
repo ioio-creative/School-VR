8f531f0d0ff473cd7b882c102c79e7f3f7b7c524
Sze Wa, Wong <szewa.wong@connect.polyu.hk>
2019-04-18 17:50:34 +0800

    working on preview button in project list page

--- src/pages/ProjectListPage.js
+++ src/pages/ProjectListPage.js
@@ -14,49 +14,104 @@ import handleErrorWithUiDefault from 'utils/errorHandling/handleErrorWithUiDefau
 import './ProjectListPage.css';
 
 
-function ProjectItem(props) {
-  const project = props.item;
-  const addFocusClass = (existingClass) =>
-    (existingClass + (props.isFocus ? " focus" : ""));
+class ProjectItem extends Component {
+  constructor(props) {
+    super(props);
 
-  function handleItemClick() {
-    invokeIfIsFunction(props.onClick);
+    this.state = {
+      isShowProjectHandles: false
+    };
   }
 
-  function handleItemDoubleClick() {
-    invokeIfIsFunction(props.onDoubleClick);
+
+  /* methods */
+
+  showProjectHandles = _ => {
+    if (!this.state.isShowProjectHandles) {
+      this.setState({
+        isShowProjectHandles: true
+      });
+    }
   }
 
-  return (    
-    <div className="project-item"
-      onClick={handleItemClick}
-      onDoubleClick={handleItemDoubleClick}
-    >
-      <div className="project-info-container">
-        <div className="project-info">
-          <div className="project-image">
-            <img src={getAbsoluteUrlFromRelativeUrl("images/test1.jpg")} alt={"test"}/>
+  hideProjectHandles = _ => {
+    if (this.state.isShowProjectHandles) {
+      this.setState({
+        isShowProjectHandles: false
+      });
+    }
+  }
+
+  /* end of methods */
+
+
+  /* event handlers */
+
+  handleItemClick = _ => {
+    invokeIfIsFunction(this.props.onClick);
+  }
+
+  handleItemDoubleClick = _ => {
+    invokeIfIsFunction(this.props.onDoubleClick);
+  }
+
+  handleItemMouseEnter = _ => {    
+    this.showProjectHandles();    
+  }
+
+  handleItemMouseLeave = _ => {
+    this.hideProjectHandles();    
+  }
+
+  /* end of event handlers */
+
+
+  render() {
+    const props = this.props;
+    const state = this.state;
+
+    const project = props.item;
+    const addFocusClass = (existingClass) =>
+      (existingClass + (props.isFocus ? " focus" : ""));
+
+    return (    
+      <div className="project-item"
+        onClick={this.handleItemClick}
+        onDoubleClick={this.handleItemDoubleClick}
+        // https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_onmousemove_leave_out     
+        onMouseEnter={this.handleItemMouseEnter}      
+        //onMouseLeave={this.handleItemMouseLeave}
+      >
+        <div className="project-info-container">
+          <div className="project-info">
+            <div className="project-image">
+              <img src={getAbsoluteUrlFromRelativeUrl("images/test1.jpg")} alt={"test"}/>
+            </div>
+            <div className="project-info-text-container">
+              <div className="project-info-text">
+                <div className="project-name">{"site_visit_2018"}</div>
+                <div className="project-lastupdate">{`Last edited 2018/11/13`}</div>
+              </div>
+            </div>
           </div>
-          <div className="project-info-text-container">
-            <div className="project-info-text">
-              <div className="project-name">{"site_visit_2018"}</div>
-              <div className="project-lastupdate">{`Last edited 2018/11/13`}</div>
+        </div>      
+        <div className={`project-handles-container ${state.isShowProjectHandles ? 'show' : 'hide'}`}>
+          <div className="project-handles">
+            <div className="project-preview-container">
+              <div className="project-preview">Preview</div>
+            </div>
+            <div className="project-options">Options</div>
+            <div className="project-edit-container">
+              <div className="project-edit">Edit</div>
             </div>
           </div>
         </div>
       </div>
-      {/* <div className="project-handles-container">
-        <div className="project-handles">
-          <div className="project-preview">{"Preview"}</div>
-          <div className="project-options">{"Options"}</div>
-          <div className="project-edit">{"Edit"}</div>
-        </div>
-      </div> */}
-    </div>
-  );
+    );
+  }
 }
 
-
+  
 function ProjectList(props) {
   const items = props.items;
 
@@ -162,12 +217,12 @@ class ProjectListPage extends Component {
 
   // Click on item
   handleProjectItemClick = (evnt, itemIdx) => {
-    if (this.state.focusedItemIdx !== itemIdx) {
-      this.setState({
-        focusedItemIdx: itemIdx
-      });
-    }
-    evnt.stopPropagation();
+    // if (this.state.focusedItemIdx !== itemIdx) {
+    //   this.setState({
+    //     focusedItemIdx: itemIdx
+    //   });
+    // }
+    // evnt.stopPropagation();
   }
 
   /**
@@ -176,9 +231,9 @@ class ProjectListPage extends Component {
    * @param {CustomedFileStats} projectFileStat 
    */
   handleProjectItemDoubleClick = (projectFileStat) => {        
-    const projectName = projectFileStat.fileNameWithoutExtension;
-    loadProjectByProjectNameAsync(projectName)
-      .catch(err => handleErrorWithUiDefault(err));
+    // const projectName = projectFileStat.fileNameWithoutExtension;
+    // loadProjectByProjectNameAsync(projectName)
+    //   .catch(err => handleErrorWithUiDefault(err));
   }
 
   // Refresh when in focus again
